2c3c2cf59eee3d99aeed478c026375b6a9c7ee02
Rachit Mittal <mittalrachit10063003@gmail.com>
2025-03-03 20:48:08 +0530

    create in role written

--- roles/views.py
+++ roles/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from .models import Roles
-from .serializers import RoleSerializers
+from .serializers import RoleSerializers 
 
 # Create your views here.
 class RoleViewSet(viewsets.ModelViewSet):
     queryset = Roles.objects.all()
     serializer_class = RoleSerializers
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, args, kwargs)
+        return Response({"message": "Role Successfully created"}, status=status.HTTP_201_CREATED)
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from .models import CustomUser
 from .serializers import UserSerializer
 
@@ -7,3 +8,7 @@ from .serializers import UserSerializer
 class UserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, args, kwargs)
+        return Response({"message":"User Added Successfully"},status=status.HTTP_201_CREATED)
