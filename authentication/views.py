4c4eadb12df2139185be77f6a9552a7a33ea0e18
Rachit Mittal <mittalrachit10063003@gmail.com>
2025-03-12 21:40:01 +0530

    register created

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-from .serializers import LoginSerializer
+from .serializers import LoginSerializer, RegisterSerializer
+from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
+from rest_framework import status
+
 
 # Create your views here.
 class LoginView:
     serializer_class = LoginSerializer
+
+class RegisterView(CreateAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, ** kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({"message":"User Created successfully."},status=status.HTTP_201_CREATED)
+        return Response({"message":"User not created","error":serializer.errors},status=status.HTTP_400_BAD_REQUEST)
